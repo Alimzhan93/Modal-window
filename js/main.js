19a697b3bccc1c11e4df77317fc91bc2489e6319
77001 <alimzhanzhakashev@gmail.com>
2023-02-01 17:12:04 +0600

    btn.forEach commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
-const btn = document.querySelector('.show-modal')
+const btn = document.querySelectorAll('.show-modal')
 const modal = document.querySelector('.modal')
 const closeModal = document.querySelector('.close-modal')
 const overlay = document.querySelector('.overlay')
 
 
-btn.addEventListener('click', function(){
-    modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
-})
+// btn.addEventListener('click', function(){
+//     modal.classList.remove('hidden')
+//     overlay.classList.remove('hidden')
+// })
 
 closeModal.addEventListener('click', function(){
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
 })
+
+// for (let i = 0; i < btn.length; i++) {
+//     btn[i].addEventListener('click',function(){
+//         modal.classList.remove('hidden')
+//         overlay.classList.remove('hidden')
+//     })
+    
+// }
+
+overlay.addEventListener('click', function (){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+  })
+
+
+  btn.forEach(function(item){
+    item.addEventListener('click',function(){
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
+  })
